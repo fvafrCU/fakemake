9844e608d7b56a8fc06da07a2bee87b647dad701
fvafrCU <fvafrcu@arcor.de>
2017-10-05 09:04:31 +0200

    add sink_all

---
+++ R/sink_all.R
@@ -0,0 +1,30 @@
+#' Divert Message And Output Stream to File
+#' 
+#' All output and messages up to the first error, for example thrown by 
+#' \code{\link{stop}}.
+#'
+#' @param path The path of the file to divert to.
+#' @param code The code to be executed.
+#' @return \code{\link[base:invisible]{Invisibly}} \code{\link{NULL}}. 
+#' @export
+#' @examples
+#' sink_path  <- file.path(tempdir(), "sink_all.txt")
+#' sink_all(sink_path, {
+#'          print("some output")
+#'          warning("a warning")
+#'          message("a message")
+#'          print("some more output")
+#' })
+#' file.show(sink_path, pager = "cat")
+sink_all <- function(path, code) {
+    op <- options(warn = 1)
+    on.exit(options(op))
+    con <- file(path)
+    sink(con, append = FALSE, type = "output")
+    on.exit(sink(file = NULL, type = "output"), add = TRUE)
+    sink(con, append = TRUE, type = "message")
+    on.exit(sink(file = NULL, type = "message"), add = TRUE)
+    on.exit(close(con), add = TRUE)
+    force(code)
+    return(invisible(NULL))
+}
