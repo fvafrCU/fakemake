9233c1e88bdd84459560bfebb066f3a0f5225921
fvafrCU <fvafrcu@arcor.de>
2017-10-10 11:45:24 +0200

    enhance unit tests

---
+++ R/internals.R
@@ -0,0 +1,9 @@
+# Thanks to Gabor Grothendieck and Josh O'Brien on
+# https://stackoverflow.com/questions/
+# 26539441/r-remove-null-elements-from-list-of-lists
+is_null <- function(x) is.null(x) | all(sapply(x, is.null))
+
+prune_list <- function(x) {
+   x <- Filter(Negate(is_null), x)
+   lapply(x, function(x) if (is.list(x)) prune_list(x) else x)
+}
---
+++ inst/runit_tests/runit-makefile.R
@@ -0,0 +1,35 @@
+test_provide_makefile <- function() {
+    result <- provide_make_list(type = "minimal")
+    expectation <- list(list(target = "all.Rout", 
+                             prerequisites = c("a1.Rout", "a2.Rout"
+                                               ), code = "print(\"all\")"), 
+                        list(target = "a2.Rout", prerequisites = NULL, 
+                             code = "print(\"a2\")"), 
+                        list(target = "a1.Rout", prerequisites = "b1.Rout", 
+                             code = "print(\"a1\")"), 
+                        list(target = "b1.Rout", prerequisites = NULL, 
+                             code = "print(\"b1\")"))
+    RUnit::checkIdentical(result, expectation)
+}
+
+test_write_makefile <- function() {
+    path <- tempfile()
+    makefile <- provide_make_list(type = "minimal")
+    write_makefile(makefile, path = path)
+    result <- readLines(path)
+    expectation <- readLines(system.file("templates", "Makefile_minimal", 
+                               package = "fakemake"))
+    RUnit::checkIdentical(result, expectation)
+}
+
+test_read_makefile <- function() {
+    path <- tempfile()
+    expectation <- provide_make_list(type = "minimal")
+    write_makefile(makefile, path = path)
+    result <- read_makefile(path)
+    prune_list(result)
+    RUnit::checkIdentical(result, expectation)
+}
+
+
+
