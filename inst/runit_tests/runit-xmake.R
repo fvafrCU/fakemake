8256fc2975e7f32c092c66e271433822f93455e1
fvafrCU <fvafrcu@arcor.de>
2017-10-16 14:30:20 +0200

    add testing for source files

--- inst/runit_tests/runit-xmake.R
+++ inst/runit_tests/runit-xmake.R
@@ -1,3 +1,5 @@
+if (interactive()) devtools::load_all()
+
 test_make_full_tree <- function() {
     ml <- fakemake:::get_ml()
     fakemake:::make_initial()
@@ -77,5 +79,36 @@ test_make_prerequisite <- function() {
     make_tree <- c("b1.Rout", "a1.Rout", "all.Rout")
     expectation <- file.path(tempdir(), make_tree)
     # TODO: somehow RUnit::checkIdentical(result, expectation) fails?!
+    RUnit::checkTrue(identical(result, expectation))
+}
+
+test_make_source_files <- function() {
+    ml <- fakemake:::get_ml()
+    fakemake:::make_initial()
+    #% add source files
+    src <- file.path(tempdir(), "src")
+    dir.create(src)
+    cat('print("foo")', file = file.path(src, "foo.R"))
+    cat('print("bar")', file = file.path(src, "bar.R"))
+    ml[[4]]["code"] <- 'lapply(list.files(src, full.names = TRUE), source)'
+    ml[[4]]["prerequisites"] <- "list.files(src, full.names = TRUE)"
+
+    #% make with updated source files
+    expectation <- ml[[4]][["target"]]
+    result <- make(ml[[4]][["target"]], ml)
+    RUnit::checkTrue(identical(result, expectation))
+
+    #% rerun 
+    # need to sleep on fast machine as the file modification times are identical
+    # otherwise.
+    Sys.sleep(1)
+    expectation <- NULL
+    result <- make(ml[[4]][["target"]], ml)
+    RUnit::checkTrue(identical(result, expectation))
+
+    #% touch source file and rerun
+    fakemake:::touch(file.path(src, "bar.R"))
+    expectation <- ml[[4]][["target"]]
+    result <- make(ml[[4]][["target"]], ml)
     RUnit::checkTrue(identical(result, expectation))
 }
