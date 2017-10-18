0dfa36405bd5e765d1dad87d5189f2be31e8f5bb
fvafrCU <fvafrcu@arcor.de>
2017-10-18 22:00:39 +0200

    running devuan

--- tests/testthat/notest-source_files.R
+++ tests/testthat/notest-source_files.R
@@ -1,23 +1,26 @@
 testthat::context("Testing source files")
-testthat::test_that("foo", 
-                    {
-                        make_list <- add_tempdir(provide_make_list(type = "minimal"))
-                        make(make_list[[1]][["target"]], make_list)
+testthat::test_that("foo", {
+                        ml <- add_tempdir(provide_ml(type = "minimal"))
+                        make(ml[[1]][["target"]], ml)
 
                         src <- file.path(tempdir(), "src")
                         dir.create(src)
                         cat('print("foo")', file = file.path(src, "foo.R"))
                         cat('print("bar")', file = file.path(src, "bar.R"))
-                        make_list[[4]]["code"] <- "lapply(list.files(src, full.names = TRUE), source)"
-                        make_list[[4]]["prerequisites"] <- "list.files(src, full.names = TRUE)"
-                        prerequisites <- make_list[[4]]["prerequisites"]
-                        prerequisites <- tryCatch(eval(parse(text = prerequisites)),
-                                                  error = function(e) return(prerequisites))
-                        for (target in prerequisites) {
-                            index <- which(lapply(make_list, "[[", "target") == target)
+                        ml[[4]]["code"] <-
+                            "lapply(list.files(src, full.names = TRUE), source)"
+                        ml[[4]]["prerequisites"] <-
+                            "list.files(src, full.names = TRUE)"
+                        pre <- ml[[4]]["prerequisites"]
+                        pre <- tryCatch(eval(parse(text = pre)),
+                                        error = function(e) return(pre))
+                        for (target in pre) {
+                            index <- which(lapply(ml,
+                                                  "[[", "target") == target)
                             if (identical(index, integer(0))) {
                                 if (! file.exists(target)) {
-                                    throw(paste0("There is no rule to make ", target, "."))
+                                    throw(paste0("There is no rule to make ",
+                                                 target, "."))
                                 } else {
                                     message("Prerequisite ", target, " found.")
                                 }
@@ -25,7 +28,7 @@ testthat::test_that("foo",
                         }
 
                         #% make with updated source files
-                        expectation <- make_list[[4]][["target"]]
-                        result <- make(make_list[[4]][["target"]], make_list)
-                        #testthat::expect_identical(result, expectation)
+                        expectation <- ml[[4]][["target"]]
+                        result <- make(ml[[4]][["target"]], ml)
+                        testthat::expect_identical(result, expectation)
 })
