7df978721609891f9decc3d393518bab0c796a20
fvafrCU <fvafrcu@arcor.de>
2017-10-19 12:01:42 +0200

    devel

--- devel.R
+++ devel.R
@@ -3,8 +3,14 @@ devtools::load_all(".")
 
 
 dependencies <- "c(\"cleanr\", \"roxygen2\")"
-dep_code <- paste("for (dep in ", dependencies, 
-                  ") if (! require(dep, character.only = TRUE))", 
+## # using packman unloads fakemake as well
+## dep_code <- paste("suppressmessages(pacman::p_unload(\"all\"));",
+##                   "for (dep in ", dependencies, ")", 
+##                   "if (! require(dep, character.only = true))", 
+##                   "install.packages(dep, repos =", 
+##                   "\"https://cran.uni-muenster.de/\")")
+dep_code <- paste("for (dep in ", dependencies, ")", 
+                  "if (! require(dep, character.only = true))", 
                   "install.packages(dep, repos =", 
                   "\"https://cran.uni-muenster.de/\")")
 cleanr_code <- paste('tryCatch(cleanr::check_directory("R/",', 
@@ -21,8 +27,8 @@ ml <- list(list(target = file.path("log", "dependencies.Rout"),
                 code = cleanr_code,
                 prerequisites = c("list.files(\"R\", full.names = TRUE)", "log/dependencies.Rout"))
 )
-print(make("log/cleanr.Rout", ml))
+print(fakemake::make("log/cleanr.Rout", ml))
 
-print(make("log/dependencies.Rout", ml))
-print(make("log/roxygen2.Rout", ml))
+print(fakemake::make("log/dependencies.Rout", ml))
+print(fakemake::make("log/roxygen2.Rout", ml))
 
