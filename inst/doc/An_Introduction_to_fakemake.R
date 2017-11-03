700d4a90c3f04e7d5fba5ebda5e147852b2af2ef
fvafrCU <fvafrcu@arcor.de>
2017-11-03 16:38:20 +0100

    working on vignette

--- inst/doc/An_Introduction_to_fakemake.R
+++ inst/doc/An_Introduction_to_fakemake.R
@@ -18,6 +18,24 @@ Sys.sleep(1)
 fakemake::touch(file.path(tempdir(), "a1.Rout"))
 withr::with_dir(tempdir(), print(fakemake::make("all.Rout", ml)))
 show_file_mtime()
+i <- which(sapply(ml, "[[", "target") == "all.Rout")
+ml[[i]]["alias"] <- "all"
+fakemake::touch(file.path(tempdir(), "b1.Rout"))
+withr::with_dir(tempdir(), print(fakemake::make("all", ml)))
+file.show(file.path(tempdir(), "b1.Rout"), pager = "cat")
+i <- which(sapply(ml, "[[", "target") == "b1.Rout")
+ml[[i]]["code"]  <- paste(ml[[i]]["code"], 
+                      "cat('hello, world', file = \"b1.Rout\")", 
+                      "print(\"foobar\")",
+                      sep = ";")
+file.remove(file.path(tempdir(), "b1.Rout"))
+withr::with_dir(tempdir(), print(fakemake::make("all.Rout", ml)))
+file.show(file.path(tempdir(), "b1.Rout"), pager = "cat")
+ml[[i]]["sink"] <- "b1.txt"
+file.remove(file.path(tempdir(), "b1.Rout"))
+withr::with_dir(tempdir(), print(fakemake::make("all.Rout", ml)))
+file.show(file.path(tempdir(), "b1.Rout"), pager = "cat")
+file.show(file.path(tempdir(), "b1.txt"), pager = "cat")
 i <- which(sapply(ml, "[[", "target") == "a1.Rout")
 ml[[i]]["code"]
 file.show(file.path(tempdir(), "a1.Rout"), pager = "cat")
