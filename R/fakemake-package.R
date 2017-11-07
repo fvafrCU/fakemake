28a17d72033fc5a2c4378f38d8ecfa7999e3c7ab
fvafrCU <fvafrcu@arcor.de>
2017-11-07 22:58:17 +0100

    enhance package comments

--- R/fakemake-package.R
+++ R/fakemake-package.R
@@ -3,6 +3,9 @@
 #' Use R as a minimal build system. This might come in handy if you are
 #' developing R packages and can not use a proper build system.
 #'
+#' You will probably want to start reading about
+#' \code{\link[fakemake:make]{make}} or reading the \cr
+#' \code{vignette("An_Introduction_to_fakemake", package = "fakemake")}.
 #'
 #' @name fakemake-package
 #' @aliases fakemake-package
