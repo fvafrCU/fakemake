fc00f84ebc228d139db7783d0fc443bb5a5c16b2
fvafrCU <fvafrcu@arcor.de>
2017-11-08 13:16:09 +0100

    clean package_makelist
    use code strings extensively, makes it more readible.

--- R/package_makelist.R
+++ R/package_makelist.R
@@ -9,7 +9,7 @@ package_makelist <- function() {
                        "print(covr::zero_coverage(co)); print(co)")
     testthat_code <- paste("tryCatch(testthat::test_package(\".\"),",
                            "error = function(e) print(e))")
-    list_of_r_files_code <- paste("grep(list.files(\".\",",
+    r_codes <- paste("grep(list.files(\".\",",
                                   "pattern = \".*\\\\.[rR]$\",",
                                   "recursive = TRUE),",
                                   "value = TRUE,",
@@ -18,13 +18,10 @@ package_makelist <- function() {
     dir_man <- "list.files(\"man\", full.names = TRUE, recursive = TRUE)"
     dir_inst <- "list.files(\"inst\", full.names = TRUE, recursive = TRUE)"
     dir_tests <- "list.files(\"tests\", full.names = TRUE, recursive = TRUE)"
-    pl <- list(list(target = file.path("log", "list_of_r_codes.txt"),
-                    prerequisites = list_of_r_files_code
-                    ),
-               list(alias = "roxygen2",
+    pl <- list(list(alias = "roxygen2",
                     target = file.path("log", "roxygen2.Rout"),
                     code = "roxygen2::roxygenize(\".\")",
-                    prerequisites = "list.files(\"R\", full.names = TRUE)"),
+                    prerequisites = dir_r),
                list(alias = "spell",
                     target = file.path("log", "spell.Rout"),
                     code = spell_code,
@@ -33,22 +30,21 @@ package_makelist <- function() {
                list(alias = "cleanr",
                     target = file.path("log", "cleanr.Rout"),
                     code = cleanr_code,
-                    prerequisites = file.path("log", "list_of_r_codes.txt")),
+                    prerequisites = r_codes),
                list(alias = "lint",
                     target = file.path("log", "lintr.Rout"),
                     code = "lintr::lint_package(path = \".\")",
-                    prerequisites = file.path("log", "list_of_r_codes.txt")),
+                    prerequisites = r_codes),
                list(alias = "testthat",
                     target = file.path("log", "testthat.Rout"),
                     code = testthat_code,
-                    prerequisites = c(file.path("log", "list_of_r_codes.txt"),
-                                      dir_tests, dir_inst)),
+                    prerequisites = c(dir_r, dir_tests, dir_inst)),
                list(alias = "covr",
                     target = file.path("log", "covr.Rout"),
                     code = covr_code,
-                    prerequisites = c(file.path("log", "list_of_r_codes.txt"),
-                                      dir_tests, dir_inst)),
-               list(alias = "build", target = "get_pkg_archive_path()",
+                    prerequisites = c(dir_r, dir_tests, dir_inst)),
+               list(alias = "build",
+                    target = "get_pkg_archive_path(absolute = FALSE)",
                     code = "devtools::build(pkg = \".\", path = \".\")",
                     sink = "log/build.Rout",
                     prerequisites = c(dir_r, dir_man,
@@ -61,6 +57,6 @@ package_makelist <- function() {
                                       "file.path(\"log\", \"roxygen2.Rout\")")),
                list(alias = "check", target = "log/check.Rout",
                     code = "check_archive_as_cran(get_pkg_archive_path())",
-                    prerequisites = "get_pkg_archive_path()"))
+                    prerequisites = "get_pkg_archive_path(absolute = FALSE)"))
     return(pl)
 }
